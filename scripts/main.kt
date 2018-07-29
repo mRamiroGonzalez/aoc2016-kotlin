40ab03691533ae61ff697c9958a6446ad4e1c171
Maxime Ramiro-Gonzalez <maxime.ramiro@hotmail.com>
2018-07-29 11:21:14 +0200

    reorganize steps 1 & 2

--- scripts/main.kt
+++ scripts/main.kt
@@ -1,8 +1,8 @@
 fun main(args: Array<String>) {
-//    println(day1("inputs/1.txt"))
-//    println(day1_2("inputs/1.txt"))
-    println(day2("inputs/2.txt"))
-    println(day2_2("inputs/2.txt"))
+    println(day1.part1("inputs/1.txt"))
+    println(day1.part2("inputs/1.txt"))
+    println(day2.part1("inputs/2.txt"))
+    println(day2.part2("inputs/2.txt"))
 }
 
 
